adcca0bf4604005ac3434c9ab2d0f0645719c900
Daniel Gordillo Rodriíguez <danielgordillo9@gmail.com>
2019-12-19 14:23:53 +0100

    Avance?

---
+++ ServerGlobitoCaliente/ServerGlobitoCaliente/Startup.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(ServerGlobitoCaliente.Startup))]
+
+namespace ServerGlobitoCaliente
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+
+            //Aqui se podria crear instaciar una clase con la info del juego y enviarsela a los jugadores
+        }
+    }
+}
