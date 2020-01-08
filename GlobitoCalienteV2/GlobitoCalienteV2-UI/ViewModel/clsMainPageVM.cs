8f4985ca974c3798b565b901264a040a18133029
Daniel Gordillo Rodriíguez <danielgordillo9@gmail.com>
2020-01-08 14:21:31 +0100

    Yeah

--- GlobitoCalienteV2/GlobitoCalienteV2-UI/ViewModel/clsMainPageVM.cs
+++ GlobitoCalienteV2/GlobitoCalienteV2-UI/ViewModel/clsMainPageVM.cs
@@ -1,27 +1,95 @@
 ﻿using Microsoft.AspNet.SignalR.Client;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace GlobitoCalienteV2_UI.ViewModel
 {
-    public class clsMainPageVM
+    public class clsMainPageVM : INotifyPropertyChanged
     {
         //Propiedades privadas
         private int _puntos;
-        private bool _partidaAcabada;
         private int _pulsaciones;
         private int _puntosRival;
+        private DelegateCommand _pulsador;
+        private DelegateCommand _enviarDatos;
         private HubConnection _conn;
         private IHubProxy _proxy;
 
+        //Constructor
         public clsMainPageVM()
         {
+            _pulsaciones = 0;
             _conn = new HubConnection("https://serverglobitocaliente.azurewebsites.net");
             _proxy = _conn.CreateHubProxy("GameHub");
             _conn.Start();
+            _proxy.On<int,int>("enviarPuntos",puntosRecibidos);
+
+            _enviarDatos = new DelegateCommand(EnviarDatos_Executed, EnviarDatos_CanExecute);
+            _pulsador = new DelegateCommand(Pulsador_Executed);
+        }
+
+        //Cosas extra
+
+        /// <summary>
+        /// Este evento se ejecuta cuando el servidor llama a enviarPuntos
+        /// </summary>
+        /// <param name="puntosJugador">Los puntos del jugador</param>
+        /// <param name="puntosRival">Los puntos del rival</param>
+        private void puntosRecibidos(int puntosJugador,int puntosRival)
+        {
+            _puntos = puntosJugador;
+            _puntosRival = puntosRival;
+            NotifyPropertyChanged("Puntos");
+            NotifyPropertyChanged("PuntosRival");
+        }
+
+        //Notify
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged(String propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        //Funciones extra
+
+        /// <summary>
+        /// Este evento se activa  al pulsar el pulsador y le suma uno a las pulsaciones
+        /// </summary>
+        private void Pulsador_Executed()
+        {
+            Pulsaciones += 1;
+            NotifyPropertyChanged("Pulsaciones");
+            NotifyPropertyChanged("EnviarDatos");
+            _enviarDatos.RaiseCanExecuteChanged();
+        }
+
+        /// <summary>
+        /// Este evento se activa  al pulsar el boton de acabar y envia los datos al servidor
+        /// </summary>
+        private void EnviarDatos_Executed()
+        {
+            _proxy.Invoke("comprobarPulsacionesValidas", Pulsaciones);
+            Pulsaciones = 0;
+            NotifyPropertyChanged("Pulsaciones");
+            _enviarDatos.RaiseCanExecuteChanged();
+        }
+
+        /// <summary>
+        /// Esta funcion nos permite saber si se puede ejecutar el boton de acabar
+        /// </summary>
+        /// <returns>Un bool que nos indica si se puede ejecutar el boton de acabar</returns>
+        private bool EnviarDatos_CanExecute()
+        {
+            bool res = false;
+            if (_pulsaciones >= 1)
+            {
+                res = true;
+            }
+            return res;
         }
 
         //Propiedades publicas
@@ -37,6 +105,42 @@ namespace GlobitoCalienteV2_UI.ViewModel
             }
         }
 
+        public IHubProxy Proxy
+        {
+            get
+            {
+                return _proxy;
+            }
+            set
+            {
+                _proxy = value;
+            }
+        }
+
+        public HubConnection Conn
+        {
+            get
+            {
+                return _conn;
+            }
+        }
+
+        public DelegateCommand Pulsador
+        {
+            get
+            {
+                return _pulsador;
+            }
+        }
+
+        public DelegateCommand EnviarDatos
+        {
+            get
+            {
+                return _enviarDatos;
+            }
+        }
+
         public int Pulsaciones
         {
             get
@@ -46,6 +150,8 @@ namespace GlobitoCalienteV2_UI.ViewModel
             set
             {
                 _pulsaciones = value;
+                NotifyPropertyChanged("EnviarDatos");
+                _enviarDatos.RaiseCanExecuteChanged();
             }
         }
 
@@ -60,20 +166,6 @@ namespace GlobitoCalienteV2_UI.ViewModel
                 _puntosRival = value;
             }
         }
-
-        public bool PartidaAcabada
-        {
-            get
-            {
-                return _partidaAcabada;
-            }
-            set
-            {
-                _partidaAcabada = value;
-            }
-        }
-
        
-
     }
 }
---
+++ ServerGlobitoCaliente/ServerGlobitoCaliente/clsGameInfo.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ServerGlobitoCaliente
+{
+    public class clsGameInfo
+    {
+        //Implementar simgleton para iniciar solo una vez o utilizar estaticos dentro
+        private int puntuacion1;
+        private int puntuacion2;
+        private int pulsacion1;
+        private int pulsacion2;
+        private string jugador1;
+        private string jugador2;
+        private object ntext;
+        private string nm;
+
+        //TODO Esto no va aqui va dentro de la clase GameInfo
+
+
+        public void enviarPuntos()
+        {
+            Clients.All.broadcastMessage(puntuacion1, puntuacion2);
+        }
+
+        /// <summary>
+        /// Nos permite conseguir las pulsaciones de los usuarios
+        /// </summary>
+        /// <param name="pulsacion">Las veces que ha pulsado el jugador</param>
+        public void conseguirPulsaciones(int pulsacion)
+        {
+
+            if (Context.ConnectionId == jugador1)
+            {
+                pulsacion1 = pulsacion;
+            }
+            else
+            {
+                pulsacion2 = pulsacion;
+            }
+            this.SumarPuntos();
+        }
+
+        /// <summary>
+        /// Suma los puntos al usuario
+        /// </summary>
+        public void SumarPuntos()
+        {
+            Random globoExplota = new Random();
+            int explota = globoExplota.Next(1, 13);
+
+            if (pulsacion1 > explota)
+            {
+                puntuacion2 += 1;
+            }
+            else if (pulsacion2 > explota)
+            {
+                puntuacion1 += 1;
+            }
+            else
+            {
+                if (pulsacion1 > pulsacion2)
+                {
+                    puntuacion1 += 1;
+                }
+                else
+                {
+                    puntuacion2 += 1;
+                }
+            }
+
+            this.enviarPuntos();
+
+        }
+
+        /// <summary>
+        /// Nos permite asignarle un id a los jugadores
+        /// </summary>
+        public void asignarJugador()
+        {
+            if (jugador1.Equals(""))
+            {
+                jugador1 = Context.ConnectionId;
+            }
+            else
+            {
+                jugador2 = nm
+                 ntext.Conn
+                    ectionId;
+            }
+           
+        }
+    }
+}
