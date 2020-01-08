e55de2d24fd9ee95708e9d86633029bd26a66df6
Daniel Gordillo Rodríguez <danielgordillo9@gmail.com>
2020-01-08 05:27:26 +0100

    Mas ayuda

---
+++ GlobitoCalienteV2/GlobitoCalienteV2-UI/ViewModel/clsMainPageVM.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNet.SignalR.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GlobitoCalienteV2_UI.ViewModel
+{
+    public class clsMainPageVM
+    {
+        //Propiedades privadas
+        private int _puntos;
+        private bool _partidaAcabada;
+        private int _pulsaciones;
+        private int _puntosRival;
+        private HubConnection _conn;
+        private IHubProxy _proxy;
+
+        public clsMainPageVM()
+        {
+            _conn = new HubConnection("https://chatsignalrderafa.azurewebsites.net");
+            _proxy = _conn.CreateHubProxy("PatoHub");
+            _conn.Start();
+        }
+
+        //Propiedades publicas
+        public int Puntos
+        {
+            get
+            {
+                return _puntos;
+            }
+            set
+            {
+                _puntos = value;
+            }
+        }
+
+        public int Pulsaciones
+        {
+            get
+            {
+                return _pulsaciones;
+            }
+            set
+            {
+                _pulsaciones = value;
+            }
+        }
+
+        public int PuntosRival
+        {
+            get
+            {
+                return _puntosRival;
+            }
+            set
+            {
+                _puntosRival = value;
+            }
+        }
+
+        public bool PartidaAcabada
+        {
+            get
+            {
+                return _partidaAcabada;
+            }
+            set
+            {
+                _partidaAcabada = value;
+            }
+        }
+
+    }
+}
--- ServerGlobitoCaliente/ServerGlobitoCaliente/GameHub.cs
+++ ServerGlobitoCaliente/ServerGlobitoCaliente/GameHub.cs
@@ -8,9 +8,55 @@ namespace ServerGlobitoCaliente
 {
     public class GameHub : Hub
     {
-        public void Hello()
+
+        private int puntuacion1;
+        private int puntuacion2;
+        private string jugador1;
+        private string jugador2;
+        public void EnviarPuntos()
         {
-            Clients.All.hello();
+            Clients.All.broadcastMessage(puntuacion1, puntuacion2);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="puntuacion"></param>
+        public void comprobarGanador(int pulsacion)
+        {
+
+            Random globoExplota = new Random();
+            int antesExplotar;
+
+            antesExplotar = globoExplota.Next(3,13);
+
+            if(antesExplotar > pulsacion)
+            {
+                if (Context.ConnectionId == jugador1)
+                {
+                    puntuacion1 += 1;
+                }
+                else
+                {
+                    puntuacion2 += 1;
+                }
+            }
+            EnviarPuntos();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public void elegirJugador()
+        {
+            if (jugador1.Equals(""))
+            {
+                jugador1 = Context.ConnectionId;
+            }
+            else
+            {
+                jugador2 = Context.ConnectionId;
+            }
         }
     }
 }
